c291bb98e55cb39723ceca9090919ca016f1b927
faheem91 <faheem91@outlook.com>
2013-11-12 04:27:37 +0500

    Update header2.php

--- application/views/common/header2.php
+++ application/views/common/header2.php
@@ -6,6 +6,6 @@
 <meta http-equiv="content-type" content="text/html;charset=UTF-8">
     <!-- Le styles -->
     
-    <?php// echo link_css('bootstrap'); ?>
+    
     <?php echo link_css('style') ?>
 <link href="<?php echo base_url(); ?>assets/css/style2.css" >
